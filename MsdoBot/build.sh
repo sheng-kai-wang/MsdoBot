da2fda4d161f6502622ba560c10cb0bd168fc492
sheng-kai-wang <nssh94879487@gmail.com>
2022-08-18 15:20:07 +0800

    [fix] msdobot's Dockerfile path

--- MsdoBot/build.sh
+++ MsdoBot/build.sh
@@ -3,5 +3,5 @@
 mvn clean install -Dmaven.test.skip=true
 cp ./target/msdobot-0.0.1-SNAPSHOT.jar app.jar
 
-docker build Dockerfile.rabbitmq -t msdobot/rabbitmq:v1
-docker build Dockerfile.msdobot -t msdobot/msdobot:v1
+docker build . -f Dockerfile.rabbitmq -t msdobot/rabbitmq:v1
+docker build . -f Dockerfile.msdobot -t msdobot/msdobot:v1
